1f8aca62cae496e809c80407a49778c8fb761416
Zain Asghar <zainasghar200@gmail.com>
2023-01-11 14:33:42 +0500

    added nave bar and side bar

---
+++ client/src/pages/CreateCampaign.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import { money } from '../assets';
+import { CustomButton } from '../components';
+import { checkIfImage } from '../utils';
+
+const CreateCampaign = () => {
+  return <div>CreateCampaign</div>;
+};
+
+export default CreateCampaign;
